dbac1bd8f9eb01be72e248280d0d07aabb71695e
Sergio <sergiobluez@gmail.com>
2023-02-16 11:18:59 -0800

    Image generator
    When API and mint button or web3 capabilities? The Coding experiments continue.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [randomImage, setRandomImage] = useState(null);
+
+  const handleClick = () => {
+    const images = [
+      'https://randomuser.me/api/portraits/men/1.jpg',
+      'https://randomuser.me/api/portraits/women/1.jpg',
+      'https://randomuser.me/api/portraits/men/2.jpg',
+      'https://randomuser.me/api/portraits/women/2.jpg',
+      'https://randomuser.me/api/portraits/men/3.jpg',
+      'https://randomuser.me/api/portraits/women/3.jpg'
+    ];
+    const index = Math.floor(Math.random() * images.length);
+    const image = images[index];
+    setRandomImage(image);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>NFT Image Generator</h1>
+        <button onClick={handleClick}>Generate Random Image</button>
+        {randomImage && <img src={randomImage} alt="NFT Image" />}
       </header>
     </div>
   );
